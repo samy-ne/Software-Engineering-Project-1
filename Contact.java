8c6219987e13fd254b19740562748c68b6ad550f
yaniv hajaj <116173143+YanivHajaj@users.noreply.github.com>
2023-05-10 18:04:42 +0300

    few changes

--- Contact.java
+++ Contact.java
@@ -2,10 +2,10 @@ public class Contact{
 	private String name;
 	private String number;
 	
-	public void SetName(String name){this.name=name};
-	public void SetNumber(String number){this.number=number};
-	public String GetName(){return name};
-	public String GetNumber(){return number};
-	public void PrintContact(){System.out.println(name + ": " number);}
+	public void SetName(String name){this.name=name;}
+	public void SetNumber(String number){this.number=number;}
+	public String GetName(){return name;}
+	public String GetNumber(){return number;};
+	public void PrintContact(){System.out.println(name + ": " + number);}
 
 }
--- Phonebook.java
+++ Phonebook.java
@@ -1,24 +1,23 @@
 import java.util.ArrayList;
-import Contact;
-
+//include Contact;
 public class Phonebook {
 	public static void main(String[] args) {
 		ArrayList<Contact> phonebook = new ArrayList<Contact>();
 		
 		// add some contacts to the phonebook
 		Contact contact1 = new Contact();
-		contact1.setName("yaniv");
-		contact1.setNumber("555-1234");
+		contact1.SetName("yaniv");
+		contact1.SetNumber("555-1234");
 		phonebook.add(contact1);
 		
 		Contact contact2 = new Contact();
-		contact2.setName("hajaj");
-		contact2.setNumber("555-5678");
+		contact2.SetName("hajaj");
+		contact2.SetNumber("555-5678");
 		phonebook.add(contact2);
 		
 		// print all contacts in the phonebook
 		for (Contact contact : phonebook) {
-			contact.printContact();
+			contact.PrintContact();
 		}
 	}
 }
